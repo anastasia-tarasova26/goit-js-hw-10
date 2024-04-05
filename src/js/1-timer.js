942291d2398f75acd38c6bd781fe1131931d578b
anastasia-tarasova26 <nastya.tarasova2682@gmail.com>
2024-04-05 17:35:52 +0300

    +

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -10,12 +10,10 @@ const days = document.querySelector('span[data-days]');
 const hours = document.querySelector('span[data-hours]');
 const minutes = document.querySelector('span[data-minutes]');
 const seconds = document.querySelector('span[data-seconds]');
-const values = document.querySelectorAll('.value'); // initiated pseudo array from elements with .value class
 
 const SECOND = 1000;
 
 let userSelectedDate = null;
-let currentTime = new Date();
 
 const options = {
   enableTime: true,
@@ -25,14 +23,11 @@ const options = {
   onClose(selectedDates) {
     selectedDates = selectedDates[0];
     userSelectedDate = selectedDates;
-    if (currentTime.getTime() < selectedDates.getTime()) {
-      // enables start button if chosen date is in the future
+    if (new Date().getTime() < selectedDates.getTime()) {
       startButton.disabled = false;
     } else {
-      // disables start button if chosen date is in the past
       startButton.disabled = true;
       izitoast.show({
-        // shhows alert window with message to choose the correct date
         message: 'Please choose a date in the future',
         messageColor: 'white',
         backgroundColor: '#e34234',
@@ -42,38 +37,28 @@ const options = {
   },
 };
 
-flatpickr('#datetime-picker', options); // calls date method
+flatpickr('#datetime-picker', options);
 
 startButton.addEventListener('click', e => {
   timerCounter();
-  startButton.disabled = true; // disabling start button after timer starts
+  startButton.disabled = true;
   startButton.style.cursor = 'not-allowed';
-  inputArea.disabled = true; // disabling input
+  inputArea.disabled = true;
   inputArea.style.cursor = 'not-allowed';
 });
 
 function timerCounter() {
   const timer = setInterval(() => {
-    // interval to repeat code every second
     let currentTime = new Date();
     let timeDifference = userSelectedDate.getTime() - currentTime.getTime();
 
     let result = convertMs(timeDifference);
 
-    // assigning results from convertMS function to text contects of elements
-    days.textContent = result.days;
-    hours.textContent = result.hours;
-    minutes.textContent = result.minutes;
-    seconds.textContent = result.seconds;
+    days.textContent = result.days.toString().padStart(2, '0');
+    hours.textContent = result.hours.toString().padStart(2, '0');
+    minutes.textContent = result.minutes.toString().padStart(2, '0');
+    seconds.textContent = result.seconds.toString().padStart(2, '0');
 
-    // checking if the number has 2 digits, if not assigning "0" before each such number
-    values.forEach(child => {
-      child.textContent =
-        child.textContent.length < 2
-          ? '0' + child.textContent
-          : child.textContent;
-    });
-    // checking if the timer came to zero and stopping the interval
     if (timeDifference <= 1000) {
       clearInterval(timer);
     }
@@ -81,18 +66,13 @@ function timerCounter() {
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const minute = SECOND * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / SECOND);
 
   return { days, hours, minutes, seconds };
